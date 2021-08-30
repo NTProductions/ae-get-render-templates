f23d6edee597d494be3095c760b98158b00e7d5e
Nate Lovell <Natelovell99@gmail.com>
2021-08-30 15:49:56 -0600

    Update Get Render Templates.jsx

--- Get Render Templates.jsx
+++ Get Render Templates.jsx
@@ -6,7 +6,7 @@ function getRenderPresetNames() {
     var tempComp = app.project.items.addComp("dummy comp", 1280, 720, 1, 30, 30);
 
     var rqItem = app.project.renderQueue.items.add(tempComp);
-    var om = rqItem.outputModules[0];
+    var om = rqItem.outputModules[1];
     var modules = om.templates;
     
     tempComp.remove();
